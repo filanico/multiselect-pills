2b7d64607a17897b49381bc2c7b12c62e37e0fd1
filanicoaccenture <filanicoaccenture@gmail.com>
2024-10-14 22:01:22 +0200

    Added pill deletion when there's not text in input

--- script.js
+++ script.js
@@ -14,23 +14,32 @@ function getPillsX(){
 	arr = arr.length > 0 ? arr : [0];
 	return arr.reduce((all,w) => all + w);
 }
+function getLastPill(){
+	return document.querySelector('.pill:last-child');
+ }
 window.onload = () => {
   [...document.querySelectorAll(".multiselect")].forEach(function (input) {
     let container = document.createElement("div");
+let typing = false;
     container.classList.add("container");
     input.parentNode.append(container);
     container.append(input);
     input.addEventListener("keyup", (e) => {
-	console.log({keyCode:e.keyCode, which:e.which});
+	let pillsX =  getPillsX();
     	if(e.keyCode == 13){
-		let pillsX =  getPillsX();
 		let input = e.target;
       		let pill = makePill(e.target.value, e.target.value.trim().toLowerCase());
 		pill.style.marginLeft = pillsX + 'px';
       		container.append(pill);
 		input.value = '';
       		input.style.paddingLeft = pillsX + pill.offsetWidth + "px";
-    	}
+    	} else if (e.keyCode == 8 && (!typing || input.value.length==0)){
+		let pill = getLastPill();
+		pill.remove();
+		input.style.paddingLeft = getPillsX() + "px";
+	} else {
+		typing = true;
+	}
     });
   });
 };
